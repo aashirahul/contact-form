08a7310abdbf26d03cde1a8861dc6ffc0d631caa
Ashima Shrivastava <ashima206@gmail.com>
2016-11-15 15:21:53 -0500

    added http for the get call

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 import angular from 'angular';
 
-angular.module('contact-formApp')
+angular.module('contact-formApp',[])
+	   .controller('FormController',['$scope','$http',function($scope,$http){
+
+	   	function getdata(){
+	   		$http({
+	   			method: 'GET',
+				 url: ' https://class-server.herokuapp.com/collections/messages'
+				}).then(function success(response){
+					console.log(response);
+				})
+	   	}
+	   	getdata();
+
+	   	
+
+
+
+	   }])
