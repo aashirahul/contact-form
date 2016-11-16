f04f0ce86ce1c5c0c391f56a65a64d7adeb1fb7f
Ashima Shrivastava <ashima206@gmail.com>
2016-11-15 21:22:45 -0500

    added submitForm method and posted data to database

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,28 @@ import angular from 'angular';
 
 angular.module('contact-formApp',[])
 	   .controller('FormController',['$scope','$http',function($scope,$http){
+	   	$scope.forms = [];
+	   	$scope.form = {};
 
 	   	function getdata(){
 	   		$http({
 	   			method: 'GET',
-				 url: ' https://class-server.herokuapp.com/collections/messages'
-				}).then(function success(response){
-					console.log(response);
+				 url: 'https://class-server.herokuapp.com/collections/arielmessages'
+				}).then(function (response){
+					$scope.forms = response.data;
+					console.log($scope.forms);	
 				})
 	   	}
 	   	getdata();
 
-	   	
+	    $scope.submitForm = function(){
+	   		$http.post('https://class-server.herokuapp.com/collections/arielmessages',$scope.form).then(function(){
+	   			getdata();
+	   			$scope.form ={};
+	   		})
 
+
+	   	}
 
 
 	   }])
